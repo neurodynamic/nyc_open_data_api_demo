666b651ca3f13f9fb681e3984da37e5b055e35be
Mark Stewart <markstewart@dbc-markstewart.local>
2016-06-22 11:07:51 -0400

    implement home page action/view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class PagesController < ApplicationController
   def home
+    nyc_vaccine_locations_url = 'https://data.cityofnewyork.us/resource/inaf-e6a5.json'
+    uri = URI.parse(nyc_vaccine_locations_url)
+    response = Net::HTTP.get_response(uri)
+
+    @json_objects = JSON.parse(response.body).first(10)
   end
 end
